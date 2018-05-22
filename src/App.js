932a1bc66ca4379ad655b15c86b68691c4c5b70c
Rayyan Karim <rkarim0598@gmail.com>
2018-05-22 12:13:26 -0400

    Add related article component

--- src/App.js
+++ src/App.js
@@ -66,34 +66,11 @@ class Advert extends Component {
     )
   }
 }
-class TheMain extends Component {
+
+class Others extends Component {
   render() {
     return (
-      <main className="expanded row">
-            <div className="large-8 medium-12 columns article">
-              <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-              <div className="avatar">
-                <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-                <div className="author-info">
-                  <p className="author-name">By Domri, son of Flug</p>
-                  <p className="date">on 28 April, 3018 of the Third Age</p>
-                </div>
-              </div>
-              <Article name="article" component={Article}/>
-              <div className="article-links">
-                <a className="article-link" href="#">
-                  <i className="fa fa-comments-o"></i>
-                  <span className="article-link-text">Comments</span>
-                </a>
-                <a className="article-link" href="#">
-                  <i className="fa fa-share"></i>
-                  <span className="article-link-text">Share Post</span>
-                </a>
-              </div>
-            </div>
-            <Advert name="advert" component={Advert}/>
-    
-            <div className="small-12 columns other-articles">
+      <div className="small-12 columns other-articles">
               <h2>From around the Realm</h2>
               <div className="small-6 medium-3 columns other-article">
                 <a href="#">
@@ -120,6 +97,37 @@ class TheMain extends Component {
                 </a> 
               </div>
             </div>
+    )
+  }
+}
+class TheMain extends Component {
+  render() {
+    return (
+      <main className="expanded row">
+            <div className="large-8 medium-12 columns article">
+              <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+              <div className="avatar">
+                <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
+                <div className="author-info">
+                  <p className="author-name">By Domri, son of Flug</p>
+                  <p className="date">on 28 April, 3018 of the Third Age</p>
+                </div>
+              </div>
+              <Article name="article" component={Article}/>
+              <div className="article-links">
+                <a className="article-link" href="#">
+                  <i className="fa fa-comments-o"></i>
+                  <span className="article-link-text">Comments</span>
+                </a>
+                <a className="article-link" href="#">
+                  <i className="fa fa-share"></i>
+                  <span className="article-link-text">Share Post</span>
+                </a>
+              </div>
+            </div>
+            <Advert name="advert" component={Advert}/>
+
+            <Others name="others" component={Others}/>
           </main>
     )
   }
