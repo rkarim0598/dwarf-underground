13369a2cb29be916e46465bdddc1366dea813241
Rayyan Karim <rkarim0598@gmail.com>
2018-05-22 12:27:17 -0400

    Add components for avatar, links, and content

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class Advert extends Component {
           <div className="ad-image">
             <img src="https://s-media-cache-ak0.pinimg.com/originals/b4/d5/ab/b4d5ab4a82b8f7efc1adaf548df1d089.jpg" alt="hammer" />
             <a href="#">visit sponsor</a>
-          </div>  
+          </div>
           <h4>Mining diamonds or crushing the skulls of your enemies, the SmashMaster 4000 does it all</h4>
           <p className="ad-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
           <p className="ad-text">Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
@@ -71,32 +71,76 @@ class Others extends Component {
   render() {
     return (
       <div className="small-12 columns other-articles">
-              <h2>From around the Realm</h2>
-              <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                  <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-                  <p>Single Orcs in Indianapolis</p>
-                </a>  
-              </div>
-              <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                  <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-                  <p>You won't believe what's under this mountain</p>
-                </a>  
-              </div>
-              <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                  <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-                  <p>Mine 20% more gold with One Weird Trick</p>
-                </a> 
-              </div>
-              <div className="small-6 medium-3 columns other-article">
-                <a href="#">
-                  <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-                  <p>Surprise for Indiana Hobbits born before 1999</p>
-                </a> 
-              </div>
-            </div>
+        <h2>From around the Realm</h2>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
+            <p>Single Orcs in Indianapolis</p>
+          </a>
+        </div>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
+            <p>You won't believe what's under this mountain</p>
+          </a>
+        </div>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
+            <p>Mine 20% more gold with One Weird Trick</p>
+          </a>
+        </div>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
+            <p>Surprise for Indiana Hobbits born before 1999</p>
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Links extends Component {
+  render() {
+    return (
+      <div className="article-links">
+        <a className="article-link" href="#">
+          <i className="fa fa-comments-o"></i>
+          <span className="article-link-text">Comments</span>
+        </a>
+        <a className="article-link" href="#">
+          <i className="fa fa-share"></i>
+          <span className="article-link-text">Share Post</span>
+        </a>
+      </div>
+    )
+  }
+}
+
+class AvatarStuff extends Component {
+  render() {
+    return (
+      <div className="avatar">
+        <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
+        <div className="author-info">
+          <p className="author-name">By Domri, son of Flug</p>
+          <p className="date">on 28 April, 3018 of the Third Age</p>
+        </div>
+      </div>
+    )
+  }
+}
+class Content extends Component {
+  render() {
+    return (
+      <div className="large-8 medium-12 columns article">
+
+        <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+        <AvatarStuff name="avatar-stuff" component={AvatarStuff}/>
+        <Article name="article" component={Article} />
+        <Links name="links" component={Links} />
+      </div>
     )
   }
 }
@@ -104,31 +148,10 @@ class TheMain extends Component {
   render() {
     return (
       <main className="expanded row">
-            <div className="large-8 medium-12 columns article">
-              <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-              <div className="avatar">
-                <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-                <div className="author-info">
-                  <p className="author-name">By Domri, son of Flug</p>
-                  <p className="date">on 28 April, 3018 of the Third Age</p>
-                </div>
-              </div>
-              <Article name="article" component={Article}/>
-              <div className="article-links">
-                <a className="article-link" href="#">
-                  <i className="fa fa-comments-o"></i>
-                  <span className="article-link-text">Comments</span>
-                </a>
-                <a className="article-link" href="#">
-                  <i className="fa fa-share"></i>
-                  <span className="article-link-text">Share Post</span>
-                </a>
-              </div>
-            </div>
-            <Advert name="advert" component={Advert}/>
-
-            <Others name="others" component={Others}/>
-          </main>
+        <Content name="content" component={Content} />
+        <Advert name="advert" component={Advert} />
+        <Others name="others" component={Others} />
+      </main >
     )
   }
 }
@@ -147,10 +170,10 @@ class TheBody extends Component {
   render() {
     return (
       <body>
-          <Menu name="menu" component={Menu}/>
-          <TheMain name="main" component={TheMain}/>
-          <TheFooter name="footer" component={TheFooter}/>
-        </body>
+        <Menu name="menu" component={Menu} />
+        <TheMain name="main" component={TheMain} />
+        <TheFooter name="footer" component={TheFooter} />
+      </body>
     )
   }
 }
@@ -163,7 +186,7 @@ class App extends Component {
       menu: ['Home', 'About', 'Log in'],
     }
   }
-  
+
   render() {
     return (
       <div classNameName="App">
